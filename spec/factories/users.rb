32fd0d7880e10b82fd1d0c8c425b0641ec8815c8
Justin Schiff <AmbientRevolution@gmail.com>
2015-01-22 03:11:05 -0800

    testing is now a go, first feature test written

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password "validpassword"
+    password_confirmation "validpassword"
   end
-
 end
---
+++ spec/features/login_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+include Warden::Test::Helpers
+Warden.test_mode!
+
+feature 'Login', js: true do
+  context 'with an already registered user' do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+    end
+
+    scenario 'sign in with valid credentials' do
+      visit '/'
+      click_button 'Login'
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      click_button 'Login!'
+
+      expect(page).to have_content('LOGOUT')
+    end
+
+    scenario 'sign in with invalid credentials' do
+      visit '/'
+      click_button 'Login'
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: "#{@user.password}invalid"
+      expect(page).to have_content('LOGIN!')
+    end
+  end
+end
